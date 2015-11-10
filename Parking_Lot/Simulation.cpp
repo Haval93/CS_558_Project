1029b566abc595ebba56dcb3ac26ee06d2035df8
Ryan Stevens <ryanstevens198@yahoo.com>
2015-11-10 14:34:42 -0800

    started the entrance arrive

--- Parking_Lot/Simulation.cpp
+++ Parking_Lot/Simulation.cpp
@@ -111,6 +111,9 @@ void Simulation_Information::timing(void)
 			minTimeNextEvent = timeOfNextEvent[i];
 			// Next event is event with smallest time
 			nextEventType = i; 
+			timeOfLastEvent = simulationTime;
+			simulationTime = timeOfNextEvent[i];
+
 		}
 	}
 
@@ -160,6 +163,42 @@ void Simulation_Information::chooseNextEvent(void)
 // Arrival Function For Entrance Gate
 void Simulation_Information::entranceArrive(void)
 {
+	// Schedule next arrival.
+	timeOfNextEvent[1] = simulationTime + massDensityFunction();
+
+	if (entranceServerStatus == BUSY) // Check to see whether server is busy.
+	{
+		// Server is busy, so increment number of customers in queue.
+		++num_in_q;
+
+		// Check to see whether an overflow condition exists.
+		if (num_in_q > Q_LIMIT)
+		{
+			/* The queue has overflowed, so stop the simulation. */
+			printf("\nOverflow of the array time_arrival at");
+			printf(" time %f", sim_time);
+			exit(2);
+		}
+
+		/* There is still room in the queue, so store the time of arrival of the
+		arriving customer at the (new) end of time_arrival. */
+		entranceQueue[num_in_q] = sim_time;
+	}
+	else // Server idle
+	{
+		/* Server is idle, so arriving customer has a delay of zero.  (The
+		following two statements are for program clarity and do not affect
+		the results of the simulation.) */
+		delay = 0.0;
+		total_of_delays += delay;
+
+		// Increment the number of customers delayed, and make server busy.
+		++num_custs_delayed;
+		server_status = BUSY;
+
+		// Schedule a departure (service completion).
+		time_next_event[2] = sim_time + serviceTime;
+	}
 
 }
 
